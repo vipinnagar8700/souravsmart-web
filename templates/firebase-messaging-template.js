5daa3551ebf1306e15062e2f19de33381456c8fd
AishwaryaFlutter <aishwarya07garg@gmail.com>
2025-08-07 22:35:37 +0530

    Some chnages

--- templates/firebase-messaging-template.js
+++ templates/firebase-messaging-template.js
@@ -6,14 +6,15 @@ importScripts(
 );
 
 firebase.initializeApp({
-  apiKey: "FIREBASE_API_KEY",
-  authDomain: "FIREBASE_AUTH_DOMAIN",
-  projectId: "FIREBASE_PROJECT_ID",
-  storageBucket: "FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID",
-  appId: "FIREBASE_APP_ID",
-  measurementId: "FIREBASE_MEASUREMENT_ID",
+  apiKey: "AIzaSyBFso7xda7jEaPhEiNg_n2J_F66Qfltx9I",
+  authDomain: "sauravmart-9a13a.firebaseapp.com",
+  projectId: "sauravmart-9a13a",
+  storageBucket: "sauravmart-9a13a.firebasestorage.app",
+  messagingSenderId: "202882851585",
+  appId: "1:202882851585:web:dfff7f4f4c901f275c3100",
+  measurementId: "G-CZE2Z8L0E8"
 });
+
 
 const messaging = firebase.messaging();
 
